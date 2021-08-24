95d3d7881c7ddd290722510028822784a2dd03f0
Julian Grisales <jgrisales@gmail.com>
2021-08-24 12:23:23 +0200

    Fetching data and CORS

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css';
 
 function App() {
 
-  const [movies, setMovies] = useState(['Movie 1', 'Movie 2']);
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/api/movies/", {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Token d85d5f44aa2fc29c458fa234d3042a14845c27a8' // Harcoded for now
+      }
+    })
+      .then(resp => resp.json())
+      .then(resp => setMovies(resp))
+      .catch(error => console.log(error))
+  }, [])
 
   return (
     <div className="App">
@@ -13,7 +26,7 @@ function App() {
       <div className="layout">
         <div>
           {movies.map(movie => {
-            return <h2>{movie}</h2>
+            return <h2>{movie.title}</h2>
           })}
         </div>
         <div>Movie detail</div>
