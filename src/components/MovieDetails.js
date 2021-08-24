336ed114d5da289568737d08c598af41ecf44b56
Julian Grisales <jgrisales@gmail.com>
2021-08-24 15:13:32 +0200

    Displaying rating

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -3,13 +3,21 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faStar } from '@fortawesome/free-solid-svg-icons'
 
 export default function MovieDetails(props) {
+
+    const movie = props.movie
+
     return (
         <div>
-            {props.movie ? (
+            {movie ? (
                 <React.Fragment>
-                    <h2>{props.movie.title}</h2>
-                    <p>{props.movie.description}</p>
-                    <FontAwesomeIcon icon={faStar} />
+                    <h2>{movie.title}</h2>
+                    <p>{movie.description}</p>
+                    <FontAwesomeIcon icon={faStar} className={movie.avg_rating > 0 ? 'orange' : ''} />
+                    <FontAwesomeIcon icon={faStar} className={movie.avg_rating > 1 ? 'orange' : ''} />
+                    <FontAwesomeIcon icon={faStar} className={movie.avg_rating > 2 ? 'orange' : ''} />
+                    <FontAwesomeIcon icon={faStar} className={movie.avg_rating > 3 ? 'orange' : ''} />
+                    <FontAwesomeIcon icon={faStar} className={movie.avg_rating > 4 ? 'orange' : ''} />
+                    ({movie.no_of_ratings})
                 </React.Fragment>
             ) : (
                 <p>No movie selected</p>
