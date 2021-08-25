b3b25c4920a592e5ed41418866f15f187e88c520
Julian Grisales <jgrisales@gmail.com>
2021-08-25 13:35:32 +0200

    Form

--- src/components/MovieForm.js
+++ src/components/MovieForm.js
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function MovieForm(props) {
 
     const movie = props.movie
+    const [title, setTitle] = useState(movie.title)
+    const [description, setDescription] = useState(movie.description)
 
     return (
         <React.Fragment>
             {movie ? (
-                <h1>{movie && movie.title} edit</h1>
+                <div>
+                    <label htmlFor="title">Title</label><br />
+                    <input
+                        id="title"
+                        type="text"
+                        placeholder="title"
+                        value={title}
+                        onChange={e => setTitle(e.target.value)}
+                    /><br />
+                    <label htmlFor="description">Description</label><br />
+                    <textarea
+                        id="description"
+                        placeholder="Description"
+                        value={description}
+                        onChange={e => setDescription(e.target.value)}>
+                    </textarea><br />
+                    
+                </div>
             ) : null}
         </React.Fragment>
     )
