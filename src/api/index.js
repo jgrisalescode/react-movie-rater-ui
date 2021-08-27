75d6173b3fe9aa1c9af7478bb730a47607e14c23
Julian Grisales <jgrisales@gmail.com>
2021-08-27 10:32:28 +0200

    Login page

--- src/api/index.js
+++ src/api/index.js
@@ -65,4 +65,14 @@ export default class API {
             })
         }).then(resp => resp.json())
     }
+
+    static login(body) {
+        return fetch(`http://127.0.0.1:8000/auth/`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(body)
+        }).then(resp => resp.json())
+    }
 }
--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import API from '../api/index'
 
 export default function Auth() {
+
+    const [username, setUserName] = useState('')
+    const [password, setPassword] = useState('')
+
+    const login = () => {
+        API.login({ username, password })
+            .then(resp => console.log(resp))
+            .catch(error => console.log(error))
+    }
+
     return (
         <div>
-            AUTH
+            <label htmlFor="username">username</label><br />
+            <input
+                id="username"
+                type="text"
+                placeholder="username"
+                value={username}
+                onChange={e => setUserName(e.target.value)}
+            /><br />
+            <label htmlFor="password">password</label><br />
+            <input
+                id="password"
+                type="password"
+                placeholder="password"
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+            /><br />
+            <button onClick={login}>Login</button>
         </div>
     )
 }
